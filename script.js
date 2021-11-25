881f2e3b64c6a7d80e0d3186be16c2dcbd5a7feb
@thiago.terceiro <thiago.terceiro@acct.global>
2021-11-24 21:17:32 -0300

    stuff

--- script.js
+++ script.js
@@ -1,93 +1,110 @@
-const holes = document.querySelectorAll('.hole');
-const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
-const coundownnBoard = document.querySelector('.countdown');
-const startButton = document.querySelector('.startButton');
-const highscore = document.querySelector('.highscore');
-const cursor = document.querySelector('.cursor img');
+const holes = document.querySelectorAll('.hole')
+const scoreBoard = document.querySelector('.score')
+const moles = document.querySelectorAll('.mole')
+const coundownnBoard = document.querySelector('.countdown')
+const startButton = document.querySelector('.startButton')
+const highscore = document.querySelector('.highscore')
+const cursor = document.querySelector('.cursor img')
+const icon = document.querySelector('.icon')
 
-
-
-let lastHole;
-let timeUp = false;
-let timeLimit = 20000;
-let score = 0;
-let countdown;
+let lastHole
+let timeUp = false
+let timeLimit = 20000
+let score = 0
+let countdown
 
 function pickRandomHole(holes) {
-
-    const RandomHole = Math.floor(Math.random() * holes.length);
-    const hole = holes[RandomHole];
-    if (hole === lastHole){
-        return pickRandomHole(holes);
-        }
-    lastHole = hole;
-    return hole;
- }
-
- function popOut() {
-     const time = Math.random() * 1200 + 400;
-     const hole = pickRandomHole(holes);
-     hole.classList.add('up');
-     setTimeout(function(){
-         hole.classList.remove('up');
-         if (!timeUp) popOut();
-    }, time);
- }
-
- //popOut(); //
-
-function startGame(){
-    countdown = timeLimit/1000;
-    scoreBoard.textContent = 0;
-    scoreBoard.style.display = 'block';
-    coundownnBoard.textContent = countdown;
-    timeUp = false;
-    score = 0;
-    popOut();
-    setTimeout(function(){
-        timeUp = true;
-        }, timeLimit);
-
-    let startCountdown = setInterval(function() {
-        countdown -= 1;
-        coundownnBoard.textContent = countdown;
-        if (countdown < 0) {
-            countdown = 0;
-            clearInterval(startCountdown);
-            coundownnBoard.textContent = 'Times Up !!';
-        }
-    }, 1000);
-    window.addEventListener("mousemove", (e) => {
-        cursor.style.top = e.pageY + "px";
-        cursor.style.left = e.pageX  + "px";
-        
-
-  window.addEventListener("click", () => {
-    cursor.style.animation = "hit 0.1s ease";
-    setTimeout(() => {
-      cursor.style.removeProperty("animation");
-    }, 100);
-  });
-});
-      }
-
-startButton.addEventListener('click', startGame);
-
-function whack(e){
-     score++;
-    this.style.backgroundImage = 'url("mole2.png")';
-    this.style.pointerEvents = 'none' ;
-    setTimeout (() =>{
-        this.style.backgroundImage = 'url("mole1.png")';
-        this.style.pointerEvents = 'all'
-    }, 900 );
-    
-
-    scoreBoard.textContent = score;
-    
+  const RandomHole = Math.floor(Math.random() * holes.length)
+  const hole = holes[RandomHole]
+  if (hole === lastHole) {
+    return pickRandomHole(holes)
+  }
+  lastHole = hole
+  return hole
 }
 
+function popOut() {
+  const time = Math.random() * 1200 + 400
+  const hole = pickRandomHole(holes)
+  hole.classList.add('up')
+  setTimeout(function () {
+    hole.classList.remove('up')
+    if (!timeUp) popOut()
+  }, time)
+}
 
-moles.forEach(mole => mole.addEventListener('click', whack));
+//popOut(); //
 
+function startGame() {
+  countdown = timeLimit / 1000
+  scoreBoard.textContent = 0
+  scoreBoard.style.display = 'block'
+  coundownnBoard.textContent = countdown
+  timeUp = false
+  score = 0
+  popOut()
+  setTimeout(function () {
+    timeUp = true
+  }, timeLimit)
+
+  let startCountdown = setInterval(function () {
+    countdown -= 1
+    coundownnBoard.textContent = countdown
+    if (countdown < 0) {
+      countdown = 0
+      clearInterval(startCountdown)
+      coundownnBoard.textContent = 'Times Up !!'
+      const score1 = scoreBoard.innerHTML
+      const currentScore = sessionStorage.getItem('currentScore')
+      if (score1 > currentScore) {
+        sessionStorage.setItem('currentScore', score1)
+        highscore.innerHTML = score
+      }
+    }
+  }, 1000)
+  // window.addEventListener('mousemove', (e) => {
+  //   cursor.style.top = e.pageY + 'px'
+  //   cursor.style.left = e.pageX + 'px'
+
+  //   window.addEventListener('click', () => {
+  //     icon.classList.toggle('hammerRotate')
+  //     console.log(icon)
+  //     // cursor.style.animation = 'hit 0.1s ease'
+  //     // setTimeout(() => {
+  //     //   cursor.style.removeProperty('animation')
+  //     // }, 100)
+  //   })
+  // })
+}
+
+startButton.addEventListener('click', startGame)
+console.log(icon)
+window.addEventListener('mousedown', () => {
+  icon.classList.add('hammerRotate')
+  console.log(icon)
+  // cursor.style.animation = 'hit 0.1s ease'
+  // setTimeout(() => {
+  //   cursor.style.removeProperty('animation')
+  // }, 100)
+})
+window.addEventListener('mouseup', () => {
+  icon.classList.remove('hammerRotate')
+  console.log(icon)
+  // cursor.style.animation = 'hit 0.1s ease'
+  // setTimeout(() => {
+  //   cursor.style.removeProperty('animation')
+  // }, 100)
+})
+function whack(e) {
+  score++
+  this.style.backgroundImage = 'url("mole2.png")'
+  this.style.pointerEvents = 'none'
+  setTimeout(() => {
+    this.style.backgroundImage = 'url("mole1.png")'
+    this.style.pointerEvents = 'all'
+  }, 900)
+
+  scoreBoard.textContent = score
+}
+
+moles.forEach((mole) => mole.addEventListener('click', whack))
